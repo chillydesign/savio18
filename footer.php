2b8d9ee21fcbd21484a3655320a65e79b8291ffe
Charles Harvey <harvey.charles@gmail.com>
2018-08-01 11:38:35 +0200

    add analytics to footer

--- footer.php
+++ footer.php
@@ -14,14 +14,16 @@
 		<?php wp_footer(); ?>
 
 		<script type="text/javascript" src="<?php echo $tdu; ?>/js/min/scripts.bundle.js?v=<?php echo wf_version(); ?>"></script>
-		<script>
-		// (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		// (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		// l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		// ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		// ga('send', 'pageview');
-		</script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123170854-2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-123170854-2');
+        </script>
+
 
 	</body>
 </html>
