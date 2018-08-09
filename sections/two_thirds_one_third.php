89aa0606e5d3d644fae9939f1dcd93aaf6042364
Mélissa Rissel <rissel.melissa@gmail.com>
2018-08-09 10:33:22 +0200

    we

--- sections/two_thirds_one_third.php
+++ sections/two_thirds_one_third.php
@@ -5,15 +5,24 @@
 <?php $text = get_sub_field('text'); ?>
 <?php $image = get_sub_field('image'); ?>
 <?php $image_side = get_sub_field('image_side'); ?>
+<?php if($image_side == 'flipped'){
+	$class_text = 'col-sm-push-5';
+	$class_image = 'col-sm-pull-7';
+} else{
+	$class_text = '';
+	$class_image = 'col-sm-push-1';
+}?>
 
 <div class="container">
 
 
-	<div class="two_thirds_one_third_flex">
-			<div class="flex_text <?php echo $image_side; ?>">
+	<div class="row">
+			<div class="col-sm-7 <?php echo $class_text; ?>">
 					<?php echo $text; ?>
 			</div>
-			<img src="<?php echo $image['sizes']['medium']; ?>" alt="" />
+			<div class="col-sm-4 <?php echo $class_image; ?>">
+				<img src="<?php echo $image['sizes']['medium']; ?>" alt="" />
+			</div>
 	</div>
 </div><!--  END OF CONTAINER -->
 
