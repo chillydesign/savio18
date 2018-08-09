6abfac65c79ecbb9333201ce1fff1db920df0557
Mélissa Rissel <rissel.melissa@gmail.com>
2018-08-09 12:08:16 +0200

    slider section

---
+++ sections/two_thirds_one_third_slider.php
@@ -0,0 +1,38 @@
+<section  class="section  section_two_thirds_one_third_slider
+<?php if(get_sub_field('increase_pad')){echo 'increase_pad';}?>">
+
+
+
+<?php $text = get_sub_field('text'); ?>
+<?php $gallery = get_field('slider'); ?>
+<?php $image_side = get_sub_field('image_side'); ?>
+<?php if($image_side == 'flipped'){
+	$class_text = 'col-sm-push-6';
+	$class_image = 'col-sm-pull-6';
+} else{
+	$class_text = '';
+	$class_image = '';
+}?>
+
+<div class="container">
+
+
+	<div class="row">
+			<div class="col-sm-6 <?php echo $class_text; ?>">
+					<?php echo $text; ?>
+			</div>
+			<div class="col-sm-6 <?php echo $class_image; ?>">
+				<div class="what_is_savio_slider_container">
+						<div class="slider">
+								<?php foreach ($what_savio_gallery as $image) : ?>
+										<div><img src="<?php echo $image['sizes']['medium']; ?>" alt="" /></div>
+								<?php endforeach; ?>
+						</div>
+				</div>
+
+			</div>
+	</div>
+</div><!--  END OF CONTAINER -->
+
+
+</section>
