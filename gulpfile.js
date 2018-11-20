3506eaaf0d4ca274c5aed17e67ec63b41120fa95
Charles Harvey <harvey.charles@gmail.com>
2018-11-20 14:02:58 +0100

    update gulp to 4

--- gulpfile.js
+++ gulpfile.js
@@ -5,43 +5,70 @@ var minify = require('gulp-minify');
 //var browserSync = require('browser-sync');
 
 
-gulp.task('heya', function() {
+// gulp.task('heya', function() {
+//   console.log('I live! Gulp is alive!');
+// });
+
+gulp.task('heya', function(done) {
   console.log('I live! Gulp is alive!');
+      done();
 });
 
 
-gulp.task('sass', function(){
+// gulp.task('sass', function(){
+//   return gulp.src('scss/global.scss')
+//     .pipe(sass()) // Converts Sass to CSS with gulp-sass
+//     .pipe(autoprefixer({
+//         cascade: false
+//     }))
+//     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//     .pipe(gulp.dest('css'))
+// });
+
+
+
+gulp.task('sass', function(done){
   return gulp.src('scss/global.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(autoprefixer({
         cascade: false
     }))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css'));
+    done();
+
 });
 
 
 
 
-gulp.task('compress', function() {
-  gulp.src('js/*.js')
-    .pipe(minify({
-        ext:{
-            src:'-debug.js',
-            min:'.js'
-        },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '-min.js' , 'jquery.js' , '.min.js' ]
-    }))
-    .pipe(gulp.dest('js/min'))
-});
+// gulp.task('compress', function() {
+//   gulp.src('js/*.js')
+//     .pipe(minify({
+//         ext:{
+//             src:'-debug.js',
+//             min:'.js'
+//         },
+//         exclude: ['tasks'],
+//         ignoreFiles: ['.combo.js', '-min.js' , 'jquery.js' , '.min.js' ]
+//     }))
+//     .pipe(gulp.dest('js/min'))
+// });
 
 
+
+
+//
+// // Run all Gulp tasks and serve application
+// gulp.task('default', ['heya', 'sass'], function() {
+//   gulp.watch('scss/**/*.scss', ['sass']);
+// //  gulp.watch('js/**/*.js',  ['compress'] );
+// });
 
 
 
 // Run all Gulp tasks and serve application
-gulp.task('default', ['heya', 'sass'], function() {
-  gulp.watch('scss/**/*.scss', ['sass']);
-//  gulp.watch('js/**/*.js',  ['compress'] );
+gulp.task('default', gulp.series( 'sass', 'heya'), function() {
+   gulp.watch('scss/**/*.scss', ['sass']);
+//  gulp.watch('js/*.js', ['compress']);
 });
