5c936c789c65d275ba7f4050366ea4c89c3c05c7
Charles Harvey <harvey.charles@gmail.com>
2018-12-03 11:45:56 +0100

    fix alignment on 404

--- 404.php
+++ 404.php
@@ -1,29 +1,26 @@
 <?php get_header(); ?>
 
 
-		<!-- section -->
-		<section class="container">
-
-			<!-- article -->
-			<article id="post-404">
-
-                <section id="welcome_section" >
-                    <div class="container">
-                        <div class="welcome_text">
-                            <h1><?php _e( 'Page not found', 'webfactor' ); ?></h1>
-            				<p>
-            					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'webfactor' ); ?></a>
-            				</p>
-                        </div>
-                    </div>
-                </section>
 
 
+<!-- article -->
+<article id="post-404">
 
-			</article>
-			<!-- /article -->
+    <section id="welcome_section" >
+        <div class="container">
+            <div class="welcome_text">
+                <h1><?php _e( 'Page not found', 'webfactor' ); ?></h1>
+                <p>
+                    <a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'webfactor' ); ?></a>
+                </p>
+            </div>
+        </div>
+    </section>
 
-		</section>
-		<!-- /section -->
+
+
+</article>
+<!-- /article -->
+
 
 <?php get_footer(); ?>
