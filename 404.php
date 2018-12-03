4ab69991602068f4d0aad5bea5cf697471127eee
Charles Harvey <harvey.charles@gmail.com>
2018-12-03 11:45:25 +0100

    updated 404 page

--- 404.php
+++ 404.php
@@ -7,10 +7,18 @@
 			<!-- article -->
 			<article id="post-404">
 
-				<h1><?php _e( 'Page not found', 'webfactor' ); ?></h1>
-				<p>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'webfactor' ); ?></a>
-				</p>
+                <section id="welcome_section" >
+                    <div class="container">
+                        <div class="welcome_text">
+                            <h1><?php _e( 'Page not found', 'webfactor' ); ?></h1>
+            				<p>
+            					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'webfactor' ); ?></a>
+            				</p>
+                        </div>
+                    </div>
+                </section>
+
+
 
 			</article>
 			<!-- /article -->
