5b2d51b6aa8d5f489737fb3ee966cfffa76c28a2
Melissa Rissel <rissel.melissa@gmail.com>
2019-09-02 08:40:56 +0200

    who we are section

---
+++ sections/who_are_we.php
@@ -0,0 +1,23 @@
+<section  class="section  section_who_are_we blue">
+<div class="container">
+	<div class="row">
+	<?php $i = 1; ?>
+	<?php while ( have_rows('person') ) : the_row(); ?>
+		<div class="person_box">
+			<div class="heading">
+				<div class="photo" style="background-image:url('<?php echo get_sub_field('photo')['url']; ?>'); background-size:cover; height: 200px; width:200px; border-radius:50%; border:solid 2px blue; display:inline-block;"></div>
+				<div class="person_name">
+					<h3><?php echo get_sub_field('name'); ?></h3>
+					<h4><?php echo get_sub_field('title'); ?></h4>
+				</div>
+			</div>
+			<hr>
+			<?php echo get_sub_field('description'); ?>
+		</div>
+
+	<?php $i++; ?>
+	<?php endwhile; ?>
+	</div> <!-- END OF ROW -->
+</div><!--  END OF CONTAINER -->
+
+</section>
