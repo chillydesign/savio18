7de745bc2b919e06ff5a2adb40a5e6c9ac95cb84
Melissa Rissel <rissel.melissa@gmail.com>
2019-11-01 13:40:27 +0100

    popup

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 
         <div id="copyright">
             <p class="container">
-            &copy; <?php echo date('Y'); ?> Savio | <a href="<?php echo $hurl;?>/information-sur-la-protection-des-donnees-personnelles" target="infosavio">Protection des données</a> | <a href="<?php echo $hurl;?>/contact" target="infosavio">Contact</a> | <a href="<?php echo $hurl;?>/conditions-generales/" target="infosavio">Conditions générales</a> | 
+            &copy; <?php echo date('Y'); ?> Savio | <a href="<?php echo $hurl;?>/information-sur-la-protection-des-donnees-personnelles" target="infosavio">Protection des données</a> | <a href="<?php echo $hurl;?>/contact" target="infosavio">Contact</a> | <a href="<?php echo $hurl;?>/conditions-generales/" target="infosavio">Conditions générales</a> |
               <a target="_blank" class="social_icon social_icon_instagram" href="https://www.instagram.com/savio.fr/"></a>
               <a target="_blank" class="social_icon social_icon_facebook" href="https://www.facebook.com/Savio-Le-français-en-samusant-389735735185222/"></a>
             <br>
@@ -16,6 +16,7 @@
 
     </footer>
 
+<div class="freetest_popup" style="display:none;"><a href="https://app.savio.fr/gratuit" target="_blank"><img alt="Accéder au test gratuit" src="<?php echo $tdu; ?>/images/Savio-popup.jpg"/></a></div>
 
 
 		<?php wp_footer(); ?>
