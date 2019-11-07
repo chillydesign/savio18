b2dc50a220b8ad602a6704993cd6b50a9b9b63ee
Melissa Rissel <rissel.melissa@gmail.com>
2019-11-07 15:32:35 +0100

    q

--- template-home.php
+++ template-home.php
@@ -41,7 +41,7 @@
             </div>
             <div class="testlink" style="display:none;">
               <a href="https://app.savio.fr/gratuit" target="_blank">
-                <img src="<?php echo get_template_directory_uri();?>/images/Savio-test-gratuit.png" alt="Testez votre niveau gratuitement!"/>
+                <img src="<?php echo get_template_directory_uri();?>/images/Savio-test-gratuit-coupe.png" alt="Testez votre niveau gratuitement!"/>
               </a>
             </div>
         </section>
