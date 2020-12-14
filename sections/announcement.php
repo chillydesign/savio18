295b85e76214b82851bb277b1bd07db84bf02363
Melissa Rissel <rissel.melissa@gmail.com>
2020-12-14 08:08:47 +0000

    oops

---
+++ sections/announcement.php
@@ -0,0 +1,9 @@
+<section  class="section  section_announcement">
+<div class="container">
+	<div class="row">
+	<?php echo get_sub_field('annonce'); ?>
+
+	</div> <!-- END OF ROW -->
+</div><!--  END OF CONTAINER -->
+
+</section>
