ff597a3fba84ba66e9c263b86e08639b066b06c6
Melissa Rissel <rissel.melissa@gmail.com>
2020-12-15 18:57:23 +0000

    fuckin hate this shit

--- footer.php
+++ footer.php
@@ -47,7 +47,7 @@
 
 						<img src="<?php echo get_template_directory_uri(); ?>/images/french-tech-logo.png" alt="French Tech in the Alps logo"/>
 
-						<span style="display: none;     position: absolute;     background: #0b6395;     width: 500px;     max-width: 50vw;     font-size: 0.8em;     line-height: 150%;     text-align: left;     padding: 5px 15px;     border: solid 2px #052f47;     top: -94px;     left: -5px;">Savio Éducation est une entreprise membre de la <strong>French Tech in the Alpes</strong>, mouvement collectif engagé pour le développement et le rayonnement de l’économie numérique.</span>
+						<span>Savio Éducation est une entreprise membre de la <strong>French Tech in the Alpes</strong>, mouvement collectif engagé pour le développement et le rayonnement de l’économie numérique.</span>
 					</a>
 			</p>
 		</div>
