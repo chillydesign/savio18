9400239b31df85075bbe05475bb25d0d1f9e78e9
Charles Harvey <harvey.charles@gmail.com>
2021-04-19 16:46:43 +0100

    as

--- sections/faq_new.php
+++ sections/faq_new.php
@@ -19,72 +19,85 @@ if ($type == 'parent') {
 
     <div class="container">
 
-        <?php foreach ($question_categories as $question_category) : ?>
+        <div class="row">
 
-            <h2 id="<?php echo  $question_category->slug;  ?>"> <?php echo $question_category->name; ?></h2>
-            <div class="questions_container">
+            <div class="col-sm-9">
 
 
-                <?php $questions = get_posts(array(
-                    'post_type' => $qu_post_type,
-                    'posts_per_page' => -1,
-                    'order' => 'DESC',
-                    'orderby' => 'menu_order',
-                    'tax_query' => array(
-                        array(
-                            'taxonomy' => $qu_tax,
-                            'field' => 'slug',
-                            'terms' => $question_category->slug
-                        )
-                    )
-                )); ?>
+                <?php foreach ($question_categories as $question_category) : ?>
 
-                <?php foreach ($questions as $question) :;  ?>
+                    <h2 id="<?php echo  $question_category->slug;  ?>"> <?php echo $question_category->name; ?></h2>
+                    <div class="questions_container">
 
-                    <div class="single_question" id="question_<?php echo $question->ID; ?>">
-                        <h2 class="single_question_title" data-question="#question_<?php echo $question->ID; ?>">
-                            <a class="single_question_link" href="<?php echo $question->guid; ?>">
-                                <?php echo $question->post_title; ?>
-                            </a>
-                        </h2>
-                        <div class="single_question_content">
 
-                            <?php echo apply_filters('the_content', $question->post_content); ?>
-                            <?php $slider = get_field('slider', $question->ID); ?>
+                        <?php $questions = get_posts(array(
+                            'post_type' => $qu_post_type,
+                            'posts_per_page' => -1,
+                            'order' => 'DESC',
+                            'orderby' => 'menu_order',
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => $qu_tax,
+                                    'field' => 'slug',
+                                    'terms' => $question_category->slug
+                                )
+                            )
+                        )); ?>
 
-                            <?php if ($slider) : ?>
-                                <div class="faq_gallery_slider">
-                                    <?php foreach ($slider as $image) : ?>
-                                        <div>
-                                            <a class="slider_gallery" href="<?php echo $image['sizes']['large']; ?>">
-                                                <img <?php if ($image['caption'] != '') {
-                                                            echo ' alt="' . $image['caption'] . '" ';
-                                                        }; ?> class="school_logo" src="<?php echo $image['sizes']['medium']; ?>" alt="" />
-                                            </a>
+                        <?php foreach ($questions as $question) :;  ?>
+
+                            <div class="single_question" id="question_<?php echo $question->ID; ?>">
+                                <h2 class="single_question_title" data-question="#question_<?php echo $question->ID; ?>">
+                                    <a class="single_question_link" href="<?php echo $question->guid; ?>">
+                                        <?php echo $question->post_title; ?>
+                                    </a>
+                                </h2>
+                                <div class="single_question_content">
+
+                                    <?php echo apply_filters('the_content', $question->post_content); ?>
+                                    <?php $slider = get_field('slider', $question->ID); ?>
+
+                                    <?php if ($slider) : ?>
+                                        <div class="faq_gallery_slider">
+                                            <?php foreach ($slider as $image) : ?>
+                                                <div>
+                                                    <a class="slider_gallery" href="<?php echo $image['sizes']['large']; ?>">
+                                                        <img <?php if ($image['caption'] != '') {
+                                                                    echo ' alt="' . $image['caption'] . '" ';
+                                                                }; ?> class="school_logo" src="<?php echo $image['sizes']['medium']; ?>" alt="" />
+                                                    </a>
+                                                </div>
+
+
+                                            <?php endforeach; ?>
                                         </div>
+                                    <?php endif; ?>
 
-
-                                    <?php endforeach; ?>
                                 </div>
-                            <?php endif; ?>
 
-                        </div>
+                            </div>
 
-                    </div>
+                        <?php endforeach; ?>
+                        <?php wp_reset_postdata(); ?>
 
+
+                    </div><!--  end of questions_container -->
                 <?php endforeach; ?>
-                <?php wp_reset_postdata(); ?>
+
+            </div>
+
+            <div class="col-sm-3">
+
+                <ul class="faq_menu">
+                    <?php foreach ($question_categories as $question_category) : ?>
+                        <li> <a href="#<?php echo  $question_category->slug;  ?>"> <?php echo $question_category->name; ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+
+            </div>
+        </div>
 
 
-            </div><!--  end of questions_container -->
-        <?php endforeach; ?>
 
     </div>
 </section>
-
-
-<ul class="faq_menu">
-    <?php foreach ($question_categories as $question_category) : ?>
-        <li> <a href="#<?php echo  $question_category->slug;  ?>"> <?php echo $question_category->name; ?></a></li>
-    <?php endforeach; ?>
-</ul>
