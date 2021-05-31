075070a55b800688d3acdf15daba47e14cb52b0b
Charles Harvey <harvey.charles@gmail.com>
2021-05-31 17:43:58 +0100

    add videos section

---
+++ sections/videos.php
@@ -0,0 +1,24 @@
+<?php $title =  get_sub_field('title'); ?>
+<section class="section section_videos">
+	<?php $column_count =  sizeof(get_sub_field('video_repeater')); ?>
+	<?php $column_class = count_to_bootstrap_class($column_count); ?>
+
+
+	<div class="container">
+		<?php if ($title) : ?>
+			<h2><?php echo $title; ?></h2>
+		<?php endif; ?>
+		<div class="row">
+			<?php while (have_rows('video_repeater')) : the_row(); ?>
+				<div class="<?php echo $column_class; ?> ">
+					<div class="column">
+						<?php $video =  get_sub_field('video'); ?>
+						<?php echo apply_filters('the_content', $video);   ?>
+					</div>
+				</div>
+			<?php endwhile; ?>
+		</div> <!-- END OF ROW -->
+
+	</div><!--  END OF CONTAINER -->
+
+</section>
