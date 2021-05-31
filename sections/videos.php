61ba71a5678c908b8be41b8ed199686721209589
Charles Harvey <harvey.charles@gmail.com>
2021-05-31 17:46:42 +0100

    ditto

--- sections/videos.php
+++ sections/videos.php
@@ -12,7 +12,11 @@
 			<?php while (have_rows('video_repeater')) : the_row(); ?>
 				<div class="<?php echo $column_class; ?> ">
 					<div class="column">
-						<?php $video =  get_sub_field('video'); ?>
+						<?php $video =  get_sub_field('link'); ?>
+						<?php $video_title =  get_sub_field('title'); ?>
+						<?php if ($video_title) : ?>
+							<h3><?php echo $video_title; ?></h3>
+						<?php endif; ?>
 						<?php echo apply_filters('the_content', $video);   ?>
 					</div>
 				</div>
