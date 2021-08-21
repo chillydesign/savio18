483d6b1c92a83d55d975081826dc7e8133331d9e
Charles Harvey <harvey.charles@gmail.com>
2021-08-21 17:36:36 +0100

    fix page background typo

--- page.php
+++ page.php
@@ -4,45 +4,46 @@
 
 
 
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <!-- article -->
-    <article id="post-<?php the_ID(); ?>" >
+        <!-- article -->
+        <article id="post-<?php the_ID(); ?>">
 
-        <?php $background = ( has_post_thumbnail() ) ? thumbnail_of_post_url( get_the_ID(), 'large' ) : false; ?>
-        <section id="welcome_section" <?php if ($background ): ?> style="background-image:url('<?php echo $background; ?>')";  <?php endif; ?> >
-            <div class="container">
-                <div class="welcome_text">
-                    <h1><?php the_title(); ?></h1>
+            <?php $background = (has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(), 'large') : false; ?>
+            <section id="welcome_section" <?php if ($background) : ?> style="background-image:url('<?php echo $background; ?>')" <?php endif; ?>>
+                <div class="container">
+                    <div class="welcome_text">
+                        <h1><?php the_title(); ?></h1>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-        <?php get_template_part('partials/breadcrumbs'); ?>
+            <?php get_template_part('partials/breadcrumbs'); ?>
 
 
-        <?php include('section-loop.php'); ?>
+            <?php include('section-loop.php'); ?>
 
-<!--
+            <!--
         <section>
             <div class="container">
-                <?php //the_content(); ?>
+                <?php //the_content(); 
+                ?>
             </div>
         </section> -->
 
 
 
-    </article>
-    <!-- /article -->
+        </article>
+        <!-- /article -->
 
-<?php endwhile; ?>
+    <?php endwhile; ?>
 
-<?php else: ?>
+<?php else : ?>
 
     <!-- article -->
     <article class="container">
 
-        <h2><?php _e( 'Sorry, nothing to display.', 'webfactor' ); ?></h2>
+        <h2><?php _e('Sorry, nothing to display.', 'webfactor'); ?></h2>
 
     </article>
     <!-- /article -->
