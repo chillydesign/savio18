72d48c83a1831ae0dceab3c1adc83b94f0aaeb3e
Charles Harvey <harvey.charles@gmail.com>
2021-12-01 11:43:21 +0000

    add english link to header

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,34 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var minify = require('gulp-minify');
-var watch = require('gulp-watch');
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require("node-sass"));
+var autoprefixer = require("gulp-autoprefixer");
+var minify = require("gulp-minify");
+var watch = require("gulp-watch");
 
-
-gulp.task('heya', function(done) {
-  console.log('I live! Gulp is alive!');
-      done();
+gulp.task("heya", function (done) {
+  console.log("I live! Gulp is alive!");
+  done();
 });
 
-
-
-
-
-gulp.task('sass', function(done){
-  return gulp.src('scss/global.scss')
+gulp.task("sass", function (done) {
+  return gulp
+    .src("scss/global.scss")
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(autoprefixer({
+    .pipe(
+      autoprefixer({
         cascade: false,
-        browsers: ['last 3 versions'],
-    }))
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'));
-    done();
-
+        browsers: ["last 3 versions"],
+      })
+    )
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("css"));
+  done();
 });
 
-gulp.task('watch', function () {
-    watch('scss/*.scss',  gulp.series( 'sass' ));
+gulp.task("watch", function () {
+  watch("scss/*.scss", gulp.series("sass"));
 });
-
-
 
 // Run all Gulp tasks and serve application
-gulp.task('default', gulp.series( 'sass', 'watch'), function() {
-    // sass function now incldes the watching of files
-
+gulp.task("default", gulp.series("sass", "watch"), function () {
+  // sass function now incldes the watching of files
 });
--- header.php
+++ header.php
@@ -1,59 +1,70 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-        <?php $tdu =  get_template_directory_uri(); ?>
-        <?php $site_name = get_bloginfo('site_name'); ?>
-        <link href="//www.google-analytics.com" rel="dns-prefetch">
+    <title><?php wp_title(''); ?><?php if (wp_title('', false)) {
+                                        echo ' :';
+                                    } ?> <?php bloginfo('name'); ?></title>
+    <?php $tdu =  get_template_directory_uri(); ?>
+    <?php $site_name = get_bloginfo('site_name'); ?>
+    <link href="//www.google-analytics.com" rel="dns-prefetch">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tdu; ?>/images/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tdu; ?>/images/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tdu; ?>/images/favicon/favicon-16x16.png">
-<link rel="manifest" href="<?php echo $tdu; ?>/images/favicon/site.webmanifest">
-<meta name="msapplication-TileColor" content="#00aba9">
-<meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tdu; ?>/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tdu; ?>/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tdu; ?>/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo $tdu; ?>/images/favicon/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="theme-color" content="#ffffff">
 
-<?php $smp = social_meta_properties(); ?>
-<!-- Open Graph -->
-<meta property="og:url" content="<?php echo $smp->url; ?>">
-<meta property="og:type" content="article" />
-<meta property="og:site_name" content="<?php echo $smp->title; ?>"/>
-<meta property="og:title" content="<?php echo $smp->title; ?>">
-<meta property="og:description" content="<?php echo $smp->description; ?>">
-<meta property="og:img" content="<?php echo $smp->image; ?>">
-<meta property="og:image" content="<?php echo $smp->image; ?>">
-<!-- <meta property="fb:admins" content="" /> -->
+    <?php $smp = social_meta_properties(); ?>
+    <!-- Open Graph -->
+    <meta property="og:url" content="<?php echo $smp->url; ?>">
+    <meta property="og:type" content="article" />
+    <meta property="og:site_name" content="<?php echo $smp->title; ?>" />
+    <meta property="og:title" content="<?php echo $smp->title; ?>">
+    <meta property="og:description" content="<?php echo $smp->description; ?>">
+    <meta property="og:img" content="<?php echo $smp->image; ?>">
+    <meta property="og:image" content="<?php echo $smp->image; ?>">
+    <!-- <meta property="fb:admins" content="" /> -->
 
-<!-- TWITTER -->
-<meta name="twitter:card" value="<?php echo $smp->description; ?>">
-<meta name="twitter:title" content="<?php echo $smp->title; ?>">
-<meta name="twitter:description" content="<?php echo $smp->description; ?>">
-<meta name="twitter:image" content="<?php echo $smp->image; ?>">
-<!-- GOOGLE -->
-<meta itemprop="name" content="<?php echo $smp->title; ?>">
-<meta itemprop="description" content="<?php echo $smp->description; ?>">
-<meta itemprop="image" content="<?php echo $smp->image; ?>">
+    <!-- TWITTER -->
+    <meta name="twitter:card" value="<?php echo $smp->description; ?>">
+    <meta name="twitter:title" content="<?php echo $smp->title; ?>">
+    <meta name="twitter:description" content="<?php echo $smp->description; ?>">
+    <meta name="twitter:image" content="<?php echo $smp->image; ?>">
+    <!-- GOOGLE -->
+    <meta itemprop="name" content="<?php echo $smp->title; ?>">
+    <meta itemprop="description" content="<?php echo $smp->description; ?>">
+    <meta itemprop="image" content="<?php echo $smp->image; ?>">
 
 
-        <meta name="apple-mobile-web-app-title" content="<?php $site_name; ?>">
-        <meta name="application-name" content="<?php $site_name; ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="<?php bloginfo('description'); ?>">
-        <?php wp_head(); ?>
+    <meta name="apple-mobile-web-app-title" content="<?php $site_name; ?>">
+    <meta name="application-name" content="<?php $site_name; ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <?php wp_head(); ?>
 
-    </head>
-    <body <?php body_class(); ?>>
+</head>
 
-        <header>
-            <div class="container">
-                <nav>
-                    <a href="<?php echo home_url(); ?>" class="branding"><?php echo $site_name; ?></a>
-                    <a href="#" id="menu_button">Menu</a>
-                    <ul id="menu_navigation">
-                        <?php chilly_nav('primary-navigation'); ?>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+<body <?php body_class(); ?>>
+
+    <header>
+        <div class="container">
+            <nav>
+                <a href="<?php echo home_url(); ?>" class="branding"><?php echo $site_name; ?></a>
+                <a href="#" id="menu_button">Menu</a>
+                <ul id="menu_navigation">
+                    <?php chilly_nav('primary-navigation'); ?>
+                </ul>
+
+                <?php if (isset($_GET['test'])) : ?>
+                    <div id="english_container">
+                        <a href="<?php echo home_url(); ?>/about-savio">About Savio (EN)</a>
+                    </div>
+                <?php endif; ?>
+
+            </nav>
+        </div>
+    </header>
