131aed0d7bfa17e8fe2e09e9df76c48b7e193abf
Charles Harvey <harvey.charles@gmail.com>
2022-04-18 16:03:25 +0200

    asdfk

--- template-authorisation-parents.php
+++ template-authorisation-parents.php
@@ -7,17 +7,21 @@ if (isset($_GET['lang'])) {
 
 
 if ($lang == 'en') {
-    $file = get_field('authorization_parents_en', 'options');
+    $file_id = get_field('authorization_parents_en', 'options');
 } else {
-    $file = get_field('authorization_parents_fr', 'options');
+    $file_id = get_field('authorization_parents_fr', 'options');
 }
 
 
+$file = get_attached_file($file_id);
 
-// Header content type
-header("Content-type: application/pdf");
+var_dump($file);
 
-header("Content-Length: " . filesize($file));
 
-// Send the file to the browser.
-readfile($file);
+// // Header content type
+// header("Content-type: application/pdf");
+
+// header("Content-Length: " . filesize($file));
+
+// // Send the file to the browser.
+// readfile($file);
