bb3b3b64a1646e98464aa1c08223f03691231413
Charles Harvey <harvey.charles@gmail.com>
2022-08-01 15:01:50 +0100

    change names of roles in footer signup mailchimp

--- partials/mailchimp_signup_form.php
+++ partials/mailchimp_signup_form.php
@@ -2,25 +2,31 @@
 <form action="https://savio.us18.list-manage.com/subscribe/post?u=9c83ec87618595ab02b9b6bc0&amp;id=818a34f0ea" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate signup_form" target="_blank" novalidate>
     <div class="form-field">
         <label for="mce-LNAME">Nom</label>
-        <input required type="text" value="" name="LNAME" class="" id="mce-LNAME"  />
+        <input required type="text" value="" name="LNAME" class="" id="mce-LNAME" />
     </div>
 
     <div class="form-field">
         <label for="mce-EMAIL">Email</label>
-        <input required type="email" value="" name="EMAIL" class="required email"  id="mce-EMAIL"  />
+        <input required type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" />
     </div>
     <div class="form-field">
         <label for="mce-ROLE">Je suis</label>
         <select required name="ROLE" class="" id="mce-ROLE">
 
+            <option value="parent">Parent</option>
+            <option value="enseignant_ecole">Enseignant / École</option>
+            <!-- 
             <option value="Parent">Parent</option>
             <option value="Enseignant / Tuteur">Enseignant / Tuteur</option>
             <option value="Ecole">Ecole</option>
+         -->
+
+
         </select>
     </div>
     <div class="form-field">
-    <label>&nbsp;</label>
-    <input type="submit" value="Envoyer" name="subscribe" id="mc-embedded-subscribe" class="button">
+        <label>&nbsp;</label>
+        <input type="submit" value="Envoyer" name="subscribe" id="mc-embedded-subscribe" class="button">
     </div>
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9c83ec87618595ab02b9b6bc0_818a34f0ea" tabindex="-1" value=""></div>
 </form>
