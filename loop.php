0785f68c5da4e09666ade09f72eb83aaa3f1a890
Charles Harvey <harvey.charles@gmail.com>
2022-10-18 09:34:01 +0100

    ditto

--- loop.php
+++ loop.php
@@ -1,31 +1,32 @@
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-
-
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
-
-
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+		<!-- article -->
+		<article style="padding: 0 20px 30px" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 
 
-	</article>
-	<!-- /article -->
+			<!-- post title -->
+			<h4>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h4>
+			<!-- /post title -->
 
-<?php endwhile; ?>
 
-<?php else: ?>
+			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php 
+			?>
+
+
+
+		</article>
+		<!-- /article -->
+
+	<?php endwhile; ?>
+
+<?php else : ?>
 
 	<!-- article -->
 	<article>
-		<h2><?php _e( 'Sorry, nothing to display.', 'webfactor' ); ?></h2>
+		<h2><?php _e('Sorry, nothing to display.', 'webfactor'); ?></h2>
 	</article>
 	<!-- /article -->
 
