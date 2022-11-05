fb93a4e19c824aa17ffe63202b15f6ab780563ed
Lil <rissel.melissa@gmail.com>
2022-11-05 15:03:11 +0000

    created double section

---
+++ sections/double_two_thirds_one_third.php
@@ -0,0 +1,34 @@
+<?php $background =  get_sub_field('background'); ?>
+
+
+<section class="section  section_double_two_thirds_one_third <?php echo $background; ?>  <?php if (get_sub_field('increase_pad')) {
+                                                                                                echo 'increase_pad';
+                                                                                            } ?>">
+
+    <?php $order = get_sub_field('order'); ?>
+    <?php $text1 = get_sub_field('text1'); ?>
+    <?php $image1 = get_sub_field('image1'); ?>
+    <?php $link1 = get_sub_field('link1'); ?>
+    <?php $text2 = get_sub_field('text2'); ?>
+    <?php $image2 = get_sub_field('image2'); ?>
+    <?php $link2 = get_sub_field('link2'); ?>
+
+
+    <div class="container">
+        <div class="unequalcols flipped">
+            <!-- <img src="<?php echo $tdu; ?>/images/beach-chair.png" alt="" /> -->
+            <img src="<?php echo $image_parents1; ?>" alt="" />
+            <div class="for_parent_text">
+                <?php echo $for_parents_paragraph_1; ?>
+            </div>
+        </div>
+        <hr />
+        <div class="unequalcols">
+            <!-- <img src="<?php echo $tdu; ?>/images/hotairballoon.png" alt="" /> -->
+            <img src="<?php echo $image_parents2; ?>" alt="" />
+            <div class="unequalcols_text">
+                <?php echo $for_parents_paragraph_2; ?>
+            </div>
+        </div>
+    </div>
+</section>
