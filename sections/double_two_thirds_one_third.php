ec3c6f544d5df1cf5f9503bdf694ab0a908f7051
Charles Harvey <harvey.charles@gmail.com>
2022-11-07 14:06:13 +0000

    add swapped

--- sections/double_two_thirds_one_third.php
+++ sections/double_two_thirds_one_third.php
@@ -12,17 +12,38 @@
     <?php $image2 = get_sub_field('image2'); ?>
     <?php $link2 = get_sub_field('link2'); ?>
 
+    <?php $swapped = get_field('swapped') ?>
+    <?php $swapped_1 = $swapped ?  '' : 'flipped'; ?>
+    <?php $swapped_2 = $swapped ?  'flipped' : ''; ?>
+
 
     <div class="container">
-        <div class="unequalcols flipped">
-            <img src="<?php echo $image1['sizes']['medium']; ?>" alt="" />
+        <div class="unequalcols <?php echo $swapped_1; ?>">
+
+            <?php if ($link1) : ?>
+                <a href="<?php echo $link1; ?>" target="_blank">
+                <?php endif; ?>
+                <img src="<?php echo $image1['sizes']['medium']; ?>" alt="" />
+                <?php if ($link1) : ?>
+                </a>
+            <?php endif; ?>
             <div class="unequalcols_text">
                 <?php echo $text1; ?>
             </div>
         </div>
         <hr />
-        <div class="unequalcols">
-            <img src="<?php echo $image2['sizes']['medium']; ?>" alt="" />
+        <div class="unequalcols <?php echo $swapped_2; ?>">
+
+            <?php if ($link1) : ?>
+                <a href="<?php echo $link1; ?>" target="_blank">
+                <?php endif; ?>
+                <img src="<?php echo $image2['sizes']['medium']; ?>" alt="" />
+
+                <?php if ($link1) : ?>
+                </a>
+            <?php endif; ?>
+
+
             <div class="unequalcols_text">
                 <?php echo $text2; ?>
             </div>
