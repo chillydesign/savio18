c54765bd01ff058e55696b27c41ccef911f08e13
Charles Harvey <harvey.charles@gmail.com>
2022-11-07 13:58:08 +0000

    adf

--- sections/double_two_thirds_one_third.php
+++ sections/double_two_thirds_one_third.php
@@ -7,17 +7,17 @@
 
     <?php $order = get_sub_field('order'); ?>
     <?php $text1 = get_sub_field('text1'); ?>
-    <?php $image1 = get_sub_field('image1')['url']; ?>
+    <?php $image1 = get_sub_field('image1'); ?>
     <?php $link1 = get_sub_field('link1'); ?>
     <?php $text2 = get_sub_field('text2'); ?>
-    <?php $image2 = get_sub_field('image2')['sizes']['medium']; ?>
+    <?php $image2 = get_sub_field('image2'); ?>
     <?php $link2 = get_sub_field('link2'); ?>
 
 
     <div class="container">
         <div class="unequalcols flipped">
             <!-- <img src="<?php echo $tdu; ?>/images/beach-chair.png" alt="" /> -->
-            <img src="<?php echo $image1; ?>" alt="" />
+            <img src="<?php echo $image1['sizes']['medium']; ?>" alt="" />
             <div class="unequalcols_text">
                 <?php echo $text1; ?>
             </div>
@@ -25,7 +25,7 @@
         <hr />
         <div class="unequalcols">
             <!-- <img src="<?php echo $tdu; ?>/images/hotairballoon.png" alt="" /> -->
-            <img src="<?php echo $image2; ?>" alt="" />
+            <img src="<?php echo $image2['sizes']['medium']; ?>" alt="" />
             <div class="unequalcols_text">
                 <?php echo $text2; ?>
             </div>
