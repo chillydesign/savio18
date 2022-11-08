038f206f9d8e1bdb2ed0550043ccffb036e606b0
Charles Harvey <harvey.charles@gmail.com>
2022-11-08 19:39:03 +0000

    add subtitle to page

--- page.php
+++ page.php
@@ -9,11 +9,15 @@
         <!-- article -->
         <article id="post-<?php the_ID(); ?>">
 
+            <?php $subtitle = get_field('subtitle'); ?>
             <?php $background = (has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(), 'large') : false; ?>
             <section class="welcome_section" <?php if ($background) : ?> style="background-image:url('<?php echo $background; ?>')" <?php endif; ?>>
                 <div class="container">
                     <div class="welcome_text">
                         <h1><?php the_title(); ?></h1>
+                        <?php if ($subtitle) : ?>
+                            <?php echo $subtitle; ?>
+                        <?php endif; ?>
                     </div>
                 </div>
             </section>
