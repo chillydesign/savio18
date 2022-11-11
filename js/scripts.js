1fc9e75cdd2b0c911665e88f16eb8e1eb8139ef3
Charles Harvey <harvey.charles@gmail.com>
2022-11-11 17:03:16 +0000

    asdf

--- js/scripts.js
+++ js/scripts.js
@@ -348,20 +348,29 @@
     //   }
     // }
     if (typeof video_id !== "undefined") {
-      var tag = document.createElement("script");
-      tag.src = "https://www.youtube.com/player_api";
-      var firstScriptTag = document.getElementsByTagName("script")[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      const ifrc = document.getElementById("video_iframe");
+      if (ifrc) {
+        ifrc.addEventListener("click", () => {
+          var tag = document.createElement("script");
+          tag.src = "https://www.youtube.com/player_api";
+          var firstScriptTag = document.getElementsByTagName("script")[0];
+          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      // Replace the 'ytplayer' element with an <iframe> and
-      // YouTube player after the API code downloads.
-      var player;
-      function onYouTubePlayerAPIReady() {
-        player = new YT.Player("video_iframe", {
-          height: "315",
-          width: "560",
-          videoId: video_id,
-          autoplay: 1,
+          console.log(firstScriptTag);
+
+          // Replace the 'ytplayer' element with an <iframe> and
+          // YouTube player after the API code downloads.
+          var player;
+          function onYouTubePlayerAPIReady() {
+            player = new YT.Player("video_iframe", {
+              height: "315",
+              width: "560",
+              videoId: video_id,
+              autoplay: 1,
+            });
+
+            console.log(player);
+          }
         });
       }
     }
