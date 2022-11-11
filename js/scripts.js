28e1e4d264e00bb078b6791857c6fd8a249e2368
Charles Harvey <harvey.charles@gmail.com>
2022-11-11 17:06:03 +0000

    asdf

--- js/scripts.js
+++ js/scripts.js
@@ -347,32 +347,30 @@
     //     });
     //   }
     // }
-    if (typeof video_id !== "undefined") {
-      const ifrc = document.getElementById("video_iframe");
-      if (ifrc) {
-        ifrc.addEventListener("click", () => {
-          var tag = document.createElement("script");
-          tag.src = "https://www.youtube.com/player_api";
-          var firstScriptTag = document.getElementsByTagName("script")[0];
-          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    const ifrc = document.getElementById("video_iframe");
+    if (ifrc) {
+      ifrc.addEventListener("click", () => {
+        var tag = document.createElement("script");
+        tag.src = "https://www.youtube.com/player_api";
+        var firstScriptTag = document.getElementsByTagName("script")[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-          console.log(firstScriptTag);
+        console.log(firstScriptTag);
 
-          // Replace the 'ytplayer' element with an <iframe> and
-          // YouTube player after the API code downloads.
-          var player;
-          function onYouTubePlayerAPIReady() {
-            player = new YT.Player("video_iframe", {
-              height: "315",
-              width: "560",
-              videoId: video_id,
-              autoplay: 1,
-            });
+        // Replace the 'ytplayer' element with an <iframe> and
+        // YouTube player after the API code downloads.
+        var player;
+        function onYouTubePlayerAPIReady() {
+          player = new YT.Player("video_iframe", {
+            height: "315",
+            width: "560",
+            videoId: "sWtVLDZWqX0",
+            autoplay: 1,
+          });
 
-            console.log(player);
-          }
-        });
-      }
+          console.log(player);
+        }
+      });
     }
   });
 })(jQuery, this);
