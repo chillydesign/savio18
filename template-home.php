c9468b9d2ef8aa16ad72c5163b9254c3ff0fdf99
Charles Harvey <harvey.charles@gmail.com>
2022-11-11 16:54:45 +0000

    asdfnkl

--- template-home.php
+++ template-home.php
@@ -61,7 +61,7 @@
                                 <div class="videocontainer">
 
                                     <div id="video_iframe">
-                                        <img src="<?php echo $tdu; ?>/youtube_preview.jpg" alt="Youtube preview">
+                                        <img src="<?php echo $tdu; ?>/images/youtube_preview.jpg" alt="Youtube preview">
                                     </div>
                                     <script>
                                         const iframe_url = "https://www.youtube-nocookie.com/embed/sWtVLDZWqX0?modestbranding=1&showinfo=0&rel=0&color=white";
