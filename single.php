691cb68aa8ec8abc133cd0929fc9000f8158d276
Charles Harvey <harvey.charles@gmail.com>
2023-03-13 12:35:43 +0000

    ditto

--- single.php
+++ single.php
@@ -1,56 +1,62 @@
 <?php get_header(); ?>
 
 
-	<!-- section -->
-	<section>
-        <div class="container">
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<section class="welcome_section">
-                <div class="container">
-                    <div class="welcome_text">
-                        <h1><?php the_title(); ?></h1>
-                    </div>
-                </div>
-            </section>
+			<section class="welcome_section">
+				<div class="container">
+					<div class="welcome_text">
+						<h1><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</section>
+
+			<!-- section -->
+			<section>
+				<div class="container">
+
+					<!-- post thumbnail -->
+					<?php if (has_post_thumbnail()) : // Check if Thumbnail exists 
+					?>
+
+						<?php the_post_thumbnail(); // Fullsize image for the single post 
+						?>
+
+					<?php endif; ?>
+					<!-- /post thumbnail -->
 
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+					<?php the_content(); // Dynamic Content 
+					?>
 
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-
-			<?php endif; ?>
-			<!-- /post thumbnail -->
-
-
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php // comments_template(); ?>
-
+					<?php // comments_template(); 
+					?>
+				</div>
+			</section>
+			<!-- /section -->
 		</article>
 		<!-- /article -->
 
 	<?php endwhile; ?>
 
-	<?php else: ?>
+<?php else : ?>
 
-		<!-- article -->
-		<article>
+	<!-- article -->
+	<article>
+		<div class="container">
+			<h1><?php _e('Sorry, nothing to display.', 'webfactor'); ?></h1>
 
-			<h1><?php _e( 'Sorry, nothing to display.', 'webfactor' ); ?></h1>
+		</div>
 
-		</article>
-		<!-- /article -->
+	</article>
+	<!-- /article -->
 
-	<?php endif; ?>
-</div>
-	</section>
-	<!-- /section -->
+<?php endif; ?>
+
 
 
 <?php get_footer(); ?>
