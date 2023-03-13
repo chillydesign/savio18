2f0578c37605e3ca110113f567dd931c7abe76c4
Charles Harvey <harvey.charles@gmail.com>
2023-03-13 15:29:45 +0000

    style index

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php get_header(); ?>
 
 
-		<!-- section -->
-		<section class="container">
 
-			<h1><?php _e( 'Latest Posts', 'webfactor' ); ?></h1>
 
-			<?php get_template_part('loop'); ?>
+<section class="welcome_section">
+	<div class="container">
+		<div class="welcome_text">
+			<h1><?php _e('Latest Posts', 'webfactor'); ?></h1>
 
-			<?php get_template_part('pagination'); ?>
+		</div>
+	</div>
+</section>
 
-		</section>
-		<!-- /section -->
+<!-- section -->
+<section class="container">
+
+	<?php get_template_part('loop'); ?>
+
+	<?php get_template_part('pagination'); ?>
+
+</section>
+<!-- /section -->
 
 
 
