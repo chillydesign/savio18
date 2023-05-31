3c8d0149b5d4e8da5a6186835b212024d195d7b4
Charles Harvey <harvey.charles@gmail.com>
2023-05-31 19:54:05 +0100

    asd

--- sections/feature_text_with_image.php
+++ sections/feature_text_with_image.php
@@ -2,6 +2,8 @@
 <?php $image = get_sub_field('image'); ?>
 <?php $img_src  = ($image)  ? $image['sizes']['large'] : ''; ?>
 <?php $image_position = get_sub_field('image_position'); ?>
+<?php $bg_src;
+$side_src; ?>
 <?php if ($image_position == 'background') {
     $bg_src = $img_src;
 } else if ($image_position == 'side') {
@@ -13,7 +15,10 @@
 <section class="welcome_section" style="background-image:url(<?php echo $bg_src; ?>)">
     <div class="container" style="position:relative;">
         <div class="welcome_text">
-            <?php echo $text; ?>
+            <div class="welcome_text_inner">
+                <?php echo $text; ?>
+            </div>
+
             <?php if ($side_src) : ?>
                 <div class="side_image" style="background-image:url(<?php echo $side_src; ?>)"></div>
             <?php endif; ?>
