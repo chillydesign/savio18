489e753f5ff5a52fb912dda07e6b1628634a0d03
Melissa Rissel <rissel.melissa@gmail.com>
2024-01-16 15:48:39 +0000

    oops

--- sections/blog_test.php
+++ sections/blog_test.php
@@ -53,7 +53,7 @@ $has_events = $events_query->have_posts();
 
         <?php var $post_number == 0; ?>
             <?php if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                <article class="single_article_loop2 <?php if($post_number % 2 == 0) {echo "even"} ?>">
+                <article class="single_article_loop2 <?php if($post_number % 2 == 0) {echo "even";} ?>">
                     <?php get_template_part('loop_single2'); ?>
                     <?php $post_number == $post_number + 1; ?>
                 </article>
