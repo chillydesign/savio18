1fb7d1d92336365dd8e93d5abc579b5ac672faa8
Charles Harvey <harvey.charles@gmail.com>
2024-02-20 09:45:57 +0000

    style tag page

--- tag.php
+++ tag.php
@@ -1,19 +1,26 @@
 <?php get_header(); ?>
 
-	
-		<!-- section -->
-		<section>
 
-			<h1><?php _e( 'Tag Archive: ', 'webfactor' ); echo single_tag_title('', false); ?></h1>
 
-			<?php get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
 
-		</section>
-		<!-- /section -->
-	
+<section class="welcome_section">
+	<div class="container">
+		<div class="welcome_text">
+			<h1><?php _e('Tag Archive: ', 'webfactor');
+				echo single_tag_title('', false); ?></h1>
 
-<?php get_sidebar(); ?>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<?php get_template_part('loop'); ?>
+
+		<?php get_template_part('pagination'); ?>
+	</div>
+</section>
+
 
 <?php get_footer(); ?>
