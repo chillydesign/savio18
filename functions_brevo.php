f2d8e02b8056828a0b7769c8f8a3b0c788ddf627
Charles Harvey <harvey.charles@gmail.com>
2024-04-05 10:27:59 +0100

    more brevo transactional api

--- functions_brevo.php
+++ functions_brevo.php
@@ -17,11 +17,22 @@ function newBrevoAPIDefault() {
 }
 
 
+
+
+
+function newBrevoAPIContacts() {
+    global $config;
+    $apiInstance = new Brevo\Client\Api\ContactsApi(
+        new GuzzleHttp\Client(),
+        $config
+    );
+    return $apiInstance;
+}
+
+
 function newBrevoAPITransactional() {
     global $config;
     $apiInstance = new Brevo\Client\Api\TransactionalEmailsApi(
-        // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
-        // This is optional, `GuzzleHttp\Client` will be used as default.
         new GuzzleHttp\Client(),
         $config
     );
@@ -37,26 +48,34 @@ function process_download_kit_2024_04() {
     $referer = $_SERVER['HTTP_REFERER'];
     $referer =  explode('?',   $referer)[0];
 
-    $role = $_POST['ROLE'];
-
     if (isset($_POST['action'])  && $_POST['action'] == 'download_kit_2024_04') {
-
         $role = $_POST['ROLE'];
+        $email = $_POST['EMAIL'];
+        $agree_to_emails = $_POST['agree_to_emails'];
         $role_string = ($role == '1') ? 'parent' : 'teacher';
-        $sendtransemail =  false; //  makeTransactionalEmailDownloadKit2024_04($role_string);
-
+        $sendtransemail =  makeTransactionalEmailDownloadKit2024_04($email, $role_string);
         if ($sendtransemail) {
-            // wp_redirect($referer . '?success', $status = 302);
+            if ($agree_to_emails == 'true') {
+                $added_to_contacts = addEmailToContacts($email, $role);
+                if ($added_to_contacts) {
+                    wp_redirect($referer . '?success=tr_co', $status = 302);
+                } else {
+                    wp_redirect($referer . '?success=tr&problem=co', $status = 302);
+                };
+            } else {
+                wp_redirect($referer . '?success=tr', $status = 302);
+            }
         } else {
-            // wp_redirect($referer . '?problem', $status = 302);
+            wp_redirect($referer . '?problem=tr', $status = 302);
         }
     } else {
-        // wp_redirect($referer . '?problem', $status = 302);
+        wp_redirect($referer . '?problem=act', $status = 302);
     }
 }
 
 
-function makeTransactionalEmailDownloadKit2024_04($type) {
+
+function makeTransactionalEmailDownloadKit2024_04($email, $type) {
     if ($type == 'parent') {
         $template_id = 92;
         $attachment_url = 'https://savio.fr/wp-content/uploads/2018/08/chevalier-e1612273795738.jpg';
@@ -65,16 +84,32 @@ function makeTransactionalEmailDownloadKit2024_04($type) {
         $attachment_url = 'https://savio.fr/wp-content/uploads/2018/08/chevalier-e1612273795738.jpg';
     }
     $apiInstance = newBrevoAPITransactional();
-    $email = [];
-    $email['to'] = array(array('email' => 'harvey.charles+new@gmail.com'));
-    $email['templateId'] = $template_id;
-    $email['attachment'] = array(array('url' => $attachment_url));
+    $recipient = [];
+    $recipient['to'] = array(array('email' => $email));
+    $recipient['templateId'] = $template_id;
+    $recipient['attachment'] = array(array('url' => $attachment_url));
     try {
-        $result = $apiInstance->sendTransacEmail($email);
+        $result = $apiInstance->sendTransacEmail($recipient);
         print_r($result);
         return true;
     } catch (Exception $e) {
         echo 'Exception when calling TransactionalEmailsApi->sendTransacEmail: ', $e->getMessage(), PHP_EOL;
+        return false;
+    }
+}
+
+function addEmailToContacts($email, $role) {
+
+    $contact = array('email' => $email, 'attributes' => array('ROLE' => $role, 'FROM_WP' => true));
+
+
+    $apiInstance = newBrevoAPIContacts();
+    try {
+        $result = $apiInstance->createContact($contact);
+        print_r($result);
+        return true;
+    } catch (Exception $e) {
+        echo 'Exception when calling ContactsApi->createContact: ', $e->getMessage(), PHP_EOL;
         return false;
     }
 }
@@ -96,6 +131,17 @@ function webfactor_download_kit_2024_04_shortcode($atts) {
 
     $output = '<div class="download_kit_2024_04_container">';
     $output .= '<form id="download_kit_2024_04" action="' . esc_url(admin_url('admin-post.php')) . ' " method="post">';
+
+
+    $output .= '  <label class="entry__label" for="EMAIL" data-required="*">Email</label>
+        <div class="entry__field">
+        <input class="input" type="text" id="EMAIL" name="EMAIL" autocomplete="off" placeholder="EMAIL" data-required="true" required />
+        </div>';
+    $output .= '<label class="entry__label" for="agree_to_emails"> Je souhaite recevoir la newsletter de Savio</label>
+        <div class="entry__field">
+        <input class="input" type="checkbox" value="true" id="agree_to_emails" name="agree_to_emails" autocomplete="off"  />
+        </div>';
+
 
     $output .= '<label class="entry__label" for="ROLE" data-required="*">Je suis</label>
                 <div class="entry__choice">
